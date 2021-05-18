1d9cfb577ba0b2c346e8c152380969be06212a21
Kajetan <kajetan@szkoladotneta.pl>
2021-05-18 22:08:23 +0200

    blazor

---
+++ MovieManagement/MovieManagement/Client/Models/DirectorDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieManagement.Client.Models
+{
+    public class DirectorDto
+    {
+        public string FullName { get; set; }
+    }
+}
